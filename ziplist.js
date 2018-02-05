384993a23c4c3b77488462ba29be3439a61242d6
git-jftorres <35513549+git-jftorres@users.noreply.github.com>
2018-02-04 21:00:48 -1000

    final commit

---
+++ ziplist.js
@@ -0,0 +1,36 @@
+listA = [1, 2, 3];
+listB = ["a", "b", "c"];
+
+function zipList (listA, listB) {
+  let returnArr = [];
+  let countA = 0;
+  let countB = 0;
+
+  for (let i = 0; i < (listA.length + listB.length); i++) {
+
+    if (i % 2 == 0) {
+
+      returnArr[i] = listA[countA];
+      countA++;
+
+    }
+    else {
+
+      returnArr[i] = listB[countB];
+      countB++;
+    }
+  }
+
+  return returnArr;
+
+}
+
+//console.log(zipList(listA, listB));
+
+function zipListTheSimpleWay (listA, listB) {
+
+  return _.flatten(_.zip(listA, listB));
+
+}
+
+console.log(zipListTheSimpleWay(listA, listB));
